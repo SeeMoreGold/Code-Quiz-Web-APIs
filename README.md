dc95c6661205d6ccc2f8754d1e8c4cf251521127
SeeMoreGold <moreau.cj@gmail.com>
2020-09-17 23:48:27 -0400

    Form for initials

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # Code-Quiz-Web-APIs
 An app that runs in the browser featuring a multiple-choice quiz testing the user's coding knowledge.
+
+https://seemoregold.github.io/Code-Quiz-Web-APIs/
+
+## Getting Started
+
+Run this application on a default browser like Google Chrome. This application is responsive and will adjust to the user's screen size. Use the dropdown navigation links on the collapsing nav bar on small screens to access the Quiz or High Score pages. Click the 'Start Quiz' button to start the quiz. Then click on the correct answer to gain a point. Click on wrong answer and lose 10 seconds. When the timer reaches 0 the quiz is over.
+
+
+## Built With
+
+* Visual Studio Code (https://code.visualstudio.com/)
+* Bootstrap (https://getbootstrap.com/)
+* JavaScript
+
+
+## Screenshots
+![Screen shot of Code Quiz home page](assets/Code-Quiz-Screenshot.png "Index screen shot")
+
+## Credits
+
+Tutorials and information on JavaScript, Bootstrap, HTML semantics and accessibility obtained from W3Schools (https://www.w3schools.com/)
+
+## Authors
+
+* **Candice Moreau** - moreau.cj@gmail.com
+
+
+## License
+
+This project is licensed under the MIT License.
+
+
+## Acknowledgments
+
+* Background image obtained from Pixabay, a free image resource. (https://www.pixabay.com)
--- assets/script.js
+++ assets/script.js
@@ -1,15 +1,25 @@
+// Here I'm declaring most of the variables
 var startButton = document.getElementById("startBtn");
 var startHeading = document.getElementById("startHeading");
 var startInfo = document.getElementById("intro");
 var questionDiv = document.getElementById("questionDiv");
 var answerDiv = document.getElementById("answerDiv");
 
+var timerDiv = document.getElementById("seconds");
+var timeLeft = 60;
+
+var score = 0;
 
 var mixQuestions;
 var currentQuesIndex;
+var wrongAlert = document.getElementById("wrong");
+var correctAlert = document.getElementById("correct");
 
+
+// When start button is clicked, the startQuiz function is called
 startButton.addEventListener('click', startQuiz);
 
+//  This function starts the quiz
 function startQuiz() {
     console.log("started");
     startButton.classList.add("hide");
@@ -23,11 +33,13 @@ function startQuiz() {
     startTimer();
 }
 
+// This function will go to the next question
 function nextQuestion() {
     clearCard();
-    askQuestion(mixQuestions[currentQuesIndex])
+    askQuestion(mixQuestions[currentQuesIndex]);
 }
 
+// displays the question and asigns the answers to the buttons
 function askQuestion(question) {
     questionDiv.innerText = question.question;
     question.answers.forEach(answer => {
@@ -35,8 +47,8 @@ function askQuestion(question) {
         button.innerText = answer.text;
         button.classList.add("button");
         if (answer.correct) {
-            button.dataset.correct = answer.correct
-        
+            button.dataset.correct = answer.correct;
+            
         }
         button.addEventListener('click', pickAnswer);
         answerDiv.appendChild(button)
@@ -44,26 +56,151 @@ function askQuestion(question) {
 }
 
 function clearCard() {
+    clearStatusClass(document.body);
     while (answerDiv.firstChild) {
         answerDiv.removeChild(answerDiv.firstChild);
     }
 
 }
 
+// this function will determine if the answer is right or wrong
 function pickAnswer(e) {
-
+    var answerChoice = e.target;
+    var correct = answerChoice.dataset.correct;
+    setStatusClass(document.body, correct);
+    Array.from(answerDiv.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct);
+    })
+    if (mixQuestions.length > currentQuesIndex + 1) {
+        nextQuestion();
+    } 
 }
 
-var questions = [
-    {question: "How do you declare a variable for an empty array named pets?",
-     answers: [
-            {text: 'var "pets" = [];', correct: false },
-            {text: 'var pets = [];', correct: false},
-            {text: 'var pets = ();', correct: true},
-            {text: 'var pets() = 0;', correct: false}
-       ]
+function setStatusClass(element, correct) {
+    clearStatusClass (element);
+    if (correct === true) {
+        correctAlert.classList.remove("hide");
+        score++;
+    } 
+    else {
+        wrongAlert.classList.remove("hide");
     }
+}
+
+// This removes the wrong or correct notification
+function clearStatusClass(element) {
+    correctAlert.classList.add("hide");
+    wrongAlert.classList.add("hide");
+}
+// This is the array of questions and answers
+var questions = [
+    {
+      question: "How do you declare a variable for an empty array named pets?",
+      answers: [
+            { text: 'var "pets" = [];', correct: false },
+            { text: 'var pets = [];', correct: true },
+            { text: 'var pets = ();', correct: false },
+            { text: 'var pets() = 0;', correct: false }
+       ]
+    },
+    {
+        question: "A variable name can only contain letters, numbers, underscores, and __________.",
+        answers: [
+              { text: 'question marks', correct: false },
+              { text: 'dollar signs', correct: true },
+              { text: 'parenthesis', correct: false },
+              { text: 'exclamation points', correct: false }
+         ]
+      },{
+        question: "What keyword removes the last element of an array?",
+        answers: [
+              { text: 'push', correct: false },
+              { text: 'shift', correct: false },
+              { text: 'unshift', correct: false },
+              { text: 'pop', correct: true }
+         ]
+      },{
+        question: "If you wanted to execute a block of code numerous times you would use a(n) __________.",
+        answers: [
+              { text: 'for loop', correct: true },
+              { text: 'break', correct: false },
+              { text: 'Boolean', correct: false },
+              { text: 'array', correct: false }
+         ]
+      },{
+        question: "In the terminal, what command is used to save your changes to the local repository?",
+        answers: [
+              { text: 'git push', correct: false },
+              { text: 'git pull', correct: false },
+              { text: 'git commit', correct: true },
+              { text: 'git clone', correct: false }
+         ]
+      },
 ]
+// This is the function to start the timer
 function startTimer() {
+    var timerInterval = setInterval(function() {
+          timeLeft--;
+          timerDiv.textContent = "Timer: " + timeLeft;
+      
+          if(timeLeft === 0) {
+            clearInterval(timerInterval);
+            endQuiz();
+          }
+      
+        }, 1000);
+      
+}
+
+// This function will take you to the initials input and the highscore page
+function endQuiz() {
+    window.open ('highscores.html','_self',false);
+    submitBtn.classList.remove("hide");
+    nameEl.classList.remove("hide");
 
 }
+
+// For the initials form
+
+var submitBtn = document.querySelector("#submit-btn");
+var initialsList = document.querySelector("#initials-list");
+var nameEl = document.querySelector("#name");
+var modalEl = document.querySelector("#modal-container");
+var modalNameEl = document.querySelector("#modal-name");
+var closeEl = document.querySelector(".close");
+
+var people = [{ name: "" }];
+var currentId = 0;
+
+function addPersonToList(event) {
+  event.preventDefault();
+  var name = nameEl.value;
+  var li = document.createElement("li");
+  li.id = people.length;
+  li.innerHTML = name;
+  people.push({ name: name });
+  initialsList.append(li);
+}
+
+function close() {
+  modalEl.style.display = "none";
+}
+
+function handleClick(event) {
+  if (event.target.matches("button")) {
+    event.preventDefault();
+    modalEl.style.display = "block";
+    currentId = parseInt(event.target.parentElement.id);
+    var name = people[currentId].name;
+    modalNameEl.textContent = name;
+    
+  }
+}
+
+submitBtn.addEventListener("click", addPersonToList);
+initialsList.addEventListener("click", handleClick);
+document.addEventListener("click", function(event) {
+  if (event.target === modalEl) {
+    close();
+  }
+});
